ba0192cd1f9b86e3a7b9823838e2e44180b4f285
naimur2 <naimurrahaman248@gmail.com>
2023-06-08 14:32:28 +0600

    gfhgfh

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -13,21 +13,21 @@ export default function Cart() {
                 {state.cart.map((item) => (
                     <CartItem key={item.id} {...item} />
                 ))}
+                {state.cart.length > 0 ? (
+                    <div className="flex justify-between items-center p-4">
+                        <h2 className="font-bold">
+                            Total: {state.cart.reduce((a, b) => a + b.price, 0)}
+                        </h2>
+                        <a className="btn btn-primary" href="/checkout">
+                            Checkout
+                        </a>
+                    </div>
+                ) : (
+                    <div className="flex justify-center items-center h-full pt-10">
+                        <h2 className=" font-bold text-2xl">Cart is empty</h2>
+                    </div>
+                )}
             </div>
-            {state.cart.length > 0 ? (
-                <div className="flex justify-between items-center p-4">
-                    <h2 className="font-bold">
-                        Total: {state.cart.reduce((a, b) => a + b.price, 0)}
-                    </h2>
-                    <a className="btn btn-primary" href="/checkout">
-                        Checkout
-                    </a>
-                </div>
-            ) : (
-                <div className="flex justify-center items-center h-full pt-10">
-                    <h2 className=" font-bold text-2xl">Cart is empty</h2>
-                </div>
-            )}
         </section>
     );
 }
