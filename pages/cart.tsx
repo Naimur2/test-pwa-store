68d2438c53156b09443b203e0f350eb140743bd5
naimur2 <naimurrahaman248@gmail.com>
2023-06-08 14:37:13 +0600

    zxdcx

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
 import CartItem from "@/components/comp/CartItem";
 import cartContext from "@/context/CartContext";
 import { useRouter } from "next/router";
@@ -13,21 +14,21 @@ export default function Cart() {
                 {state.cart.map((item) => (
                     <CartItem key={item.id} {...item} />
                 ))}
-                {state.cart.length > 0 ? (
-                    <div className="flex justify-between items-center p-4">
-                        <h2 className="font-bold">
-                            Total: {state.cart.reduce((a, b) => a + b.price, 0)}
-                        </h2>
-                        <a className="btn btn-primary" href="/checkout">
-                            Checkout
-                        </a>
-                    </div>
-                ) : (
-                    <div className="flex justify-center items-center h-full pt-10">
-                        <h2 className=" font-bold text-2xl">Cart is empty</h2>
-                    </div>
-                )}
             </div>
+            {state.cart.length > 0 ? (
+                <div className="flex justify-between items-center p-4">
+                    <h2 className="font-bold">
+                        Total: {state.cart.reduce((a, b) => a + b.price, 0)}
+                    </h2>
+                    <a className="btn btn-primary" href="/checkout">
+                        Checkout
+                    </a>
+                </div>
+            ) : (
+                <div className="flex justify-center items-center h-full pt-10">
+                    <h2 className=" font-bold text-2xl">Cart is empty</h2>
+                </div>
+            )}
         </section>
     );
 }
